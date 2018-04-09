838576361f890bc10eea8dfcee7db0918466591c
raghuvanhi Ashutosh Kumar <uniqueraghuvanshi@gmail.com>
2018-04-09 16:23:28 +0530

    Added while loop

--- Welcome.java
+++ Welcome.java
@@ -7,6 +7,20 @@ System.out.println("Welcome Ashutosh");
 System.out.println("This is Master branch");
 
 
+
+int i=1;
+while(i<=10)
+{
+	System.out.println("i is="+i);
+	System.out.println("Helloooooooooooo");
+	i++;
+}
+
+
+
+
+
+
 }
 
 }
